8a82ac6eae756a1ea6a1cb608df90479936e47a7
Kyle McCullen <kmccullen@protonmail.com>
2025-02-22 19:58:32 -0500

    refactor: add create clipboard item functions for object creation

--- src/app/shared/models/clipboard-item.ts
+++ src/app/shared/models/clipboard-item.ts
@@ -1,10 +1,20 @@
 import { Bolt11Details } from "./lightning";
 import { Wallet } from "./wallet.model";
 
+export enum ClipboardItemType {
+    Default = 'Default',
+    Address = 'Address',
+    Payment = 'Payment',
+    Bolt11 = 'Bolt11',
+    PublicKey = 'Public Key',
+    PrivateKey = 'Private Key'
+}
+
 export interface ClipboardItem {
     name: string | null;
     value: string | null;
     private: boolean;
+    type: ClipboardItemType
 }
 
 export interface AddressClipboardItem extends ClipboardItem {
@@ -21,3 +31,30 @@ export interface PaymentClipboardItem extends ClipboardItem {
 
 export interface Bolt11ClipboardItem extends ClipboardItem, Bolt11Details {
 }
+
+export const createClipboardItem = (partial: Partial<ClipboardItem>): ClipboardItem => {
+    return {
+        type: ClipboardItemType.Default,
+        private: false,
+        ...partial,
+    } as ClipboardItem;
+};
+
+export const createAddressClipboardItem = (partial: Partial<AddressClipboardItem>): AddressClipboardItem => {
+    return {
+        name: 'Bitcoin Address',
+        ...partial,
+        value: partial.address,
+        type: ClipboardItemType.Address,
+        private: false
+    } as AddressClipboardItem;
+};
+
+export const createBolt11ClipboardItem = (partial: Partial<Bolt11ClipboardItem>): Bolt11ClipboardItem => {
+    return {
+        ...partial,
+        name: 'Lightning Address',
+        type: ClipboardItemType.Bolt11,
+        private: false
+    } as Bolt11ClipboardItem;
+};
